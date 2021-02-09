c1c28dbfd829ef70e7e535a34149fbe89a95d04b
yukikosensei <68828534+yukikosensei@users.noreply.github.com>
2021-02-09 14:01:22 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click',()=> {
+    const n = Math.floor(Math.random() * 3);
+
+    switch (n) {
+      case 0:
+        btn.textContent = "大吉";
+        break;
+      case 1:
+        btn.textContent = "中吉";
+        break;
+      case 2:
+        btn.textContent = "凶";
+        break;
+    }
+  });
+}
